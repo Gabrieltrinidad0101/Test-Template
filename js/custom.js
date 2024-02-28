f784fc3e39f41cb1d03c0e3e59b6541c55271bd0
gabriel <gabrielqwes123@gmail.com>
2024-02-28 14:03:23 -0400

    fix: iphone

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,11 @@
-const sections = document.getElementById("sections")
 const partsHouse = document.querySelectorAll(".parts-house")
 const indicatorsSlide = document.querySelectorAll(".indicators-slide")
 const carouselsItem = document.querySelectorAll(".carousel-item")
+const sections = document.getElementById("sections")
+const deviceVideo = document.getElementById("deviceVideo")
+const iphoneVideo = document.getElementById("iphoneVideo")
+
+const videoId = "ByBkOs_3qk4"
 
 function iniciarMap() {
   var coord = { lat: -34.5956145, lng: -58.4431949 };
@@ -11,65 +15,86 @@ function iniciarMap() {
   });
 }
 
-const videoId = "ByBkOs_3qk4"
-
-const redirectIphone = ()=>{
-  if(!/iPhone/i.test(navigator.userAgent)) return 
-  console.log(document.getElementById("video"))
-  document.getElementById("video").addEventListener("click",(e)=>{
-    e.preventDefault();
-    //assumes that by forcing window to go to youtube will kick up option to open in app where experience works - plays my full showreel
-    window.location = "https://www.youtube.com/watch?v=0x16ngo8xfY&list=PLzSXIFcDqpiCiKXMtXtVIHnmor9uUsEhC&autoplay=1";
-  })
-
-}
-
-function onYouTubeIframeAPIReady() {
-
-    const player = new YT.Player('video', {
-        videoId,
-        playerVars: {
-          modestbranding: 1,
-          showinfo: 0,
-          rel: 0,
-          iv_load_policy: 3,
-          autoplay: 0, // Autoplay is disabled
-        },
-        events: {
-          // Optional event handlers
-          'onReady': redirectIphone,
-      }
-    });
-
-    sections.addEventListener("change",(e)=>{
-      player.seekTo(e.target.value);
-    })
-
-    partsHouse.forEach(part=>{
-        part.addEventListener("click",()=>{
-            const value = part.getAttribute("value")
-            player.seekTo(value)
-        })
-    })
-
-    const iphone = document.querySelector(".iphone")
-    if((/iPhone/i.test(navigator.userAgent))){
-      iphone?.addEventListener("click",()=>{
-        window.location = `https://www.youtube.com/watch?v=${videoId}`
-      })
-    }else{
-      iphone.style.display = "none"
-    }
-
-}
-
-
-indicatorsSlide.forEach(indicatorSlide=>{
-  indicatorSlide.addEventListener("click",()=>{
-    carouselsItem.forEach(element=>element.classList.remove("active"))
+indicatorsSlide.forEach(indicatorSlide => {
+  indicatorSlide.addEventListener("click", () => {
+    carouselsItem.forEach(element => element.classList.remove("active"))
     carouselsItem[indicatorSlide.getAttribute("index")].classList.add("active")
   })
 })
 
 
-// Check if the user agent contains 'iPhone' to detect iPhones
+// Function to dynamically load a script
+function loadScript(url) {
+  return new Promise((resolve, reject) => {
+    const script = document.createElement('script');
+    script.src = url;
+    script.onload = resolve;
+    script.onerror = reject;
+    document.head.appendChild(script);
+  });
+}
+
+const Iphone = async () => {
+  try {
+    await loadScript('https://vjs.zencdn.net/5.10.4/video.js');
+    await loadScript('https://cdnjs.cloudflare.com/ajax/libs/three.js/r76/three.js');
+    await loadScript('https://rawgit.com/yanwsh/videojs-panorama/master/dist/videojs-panorama.v5.js');
+
+    iphoneVideo.classList.remove("d-none")
+    const options = {
+      plugins: {
+        panorama: {
+          clickAndDrag: true,
+          clickToToggle: true,
+          autoMobileOrientation: true,
+          backToVerticalCenter: false,
+          backToHorizonCenter: false
+        }
+      }
+    };
+    videojs('iphoneVideo', options, function () { });
+  } catch (error) {
+    console.error('Error loading scripts:', error);
+  }
+}
+
+
+const AllDeviceExpectIphone = async () => {
+  try {
+    await loadScript('https://www.youtube.com/iframe_api');
+  } catch (error) {
+    console.error('Error loading scripts:', error);
+  }
+}
+
+
+if ((/iPhone/i.test(navigator.userAgent))) {
+  Iphone()
+} else {
+  AllDeviceExpectIphone()
+}
+
+function onYouTubeIframeAPIReady() {
+  deviceVideo.classList.remove("d-none")
+  const player = new YT.Player('deviceVideo', {
+    videoId,
+    playerVars: {
+      modestbranding: 1,
+      showinfo: 0,
+      rel: 0,
+      iv_load_policy: 3,
+      autoplay: 0, // Autoplay is disabled
+    }
+  });
+
+  sections.addEventListener("change", (e) => {
+    player.seekTo(e.target.value);
+  })
+
+  partsHouse.forEach(part => {
+    part.addEventListener("click", () => {
+      const value = part.getAttribute("value")
+      player.seekTo(value)
+    })
+  })
+}
